009beef1b534775ff38e557f394e719f6438e55e
Lucas Weidas <lucasweidas@gmail.com>
2023-04-01 15:29:37 -0300

    Configure vite src and set new tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./src/index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        leagueSpartan: ['League Spartan', 'sans-serif'],
+      },
+      screens: {
+        xsm: '350px',
+      },
+    },
   },
   plugins: [],
 };
